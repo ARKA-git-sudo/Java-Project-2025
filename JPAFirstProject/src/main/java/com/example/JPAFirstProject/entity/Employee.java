49e061f94684e421443766d1457d78556cf68535
Arka <arkamondal1298@gmail.com>
2025-07-05 12:25:57 +0530

    Add files via upload

---
+++ JPAFirstProject/src/main/java/com/example/JPAFirstProject/entity/Employee.java
@@ -0,0 +1,91 @@
+package com.example.JPAFirstProject.entity;
+
+
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "emp_name")
+    private String name;
+
+    @Column(name = "emp_dept")
+    private String department;
+
+    @Column(name = "emp_salary")
+    private double salary;
+
+
+    @Column(name = "emp_add")
+    private String address;
+
+    public Employee() {
+
+    }
+
+    public Employee(int id, String name, String department, double salary, String address) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
